a3df55d5d1ad4de50b35a62207fb7ec10a350eec
Demix88 <skrasnauskis@gmail.com>
2022-03-21 19:15:12 +0000

    Add project files.

---
+++ methodAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a number to add");
+            int z = math.MyAdd(5, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Addition result is : "+z);
+            Console.WriteLine("Please select a number to substract");
+            int a = math.MySub(5, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Substraction result is : "+a);
+            Console.WriteLine("Please select a number to multiply");
+            int b = math.MyMult(5, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Multiplication result is : "+b);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodAssignment/math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssignment
+{
+   public class math
+    {
+        public static int MyAdd(int x, int y)
+        {
+            return x + y;
+        }
+        public static int MySub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int MyMult(int x, int y)
+        {
+            return x * y;
+        }
+
+
+    }
+}
